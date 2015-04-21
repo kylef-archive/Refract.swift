7468bebe40881c538ed7d4c5d4913c4c008ed579
Kyle Fuller <kyle@fuller.li>
2015-04-20 22:27:10 -0700

    Initial Commit

---
+++ Refract/Refract.h
@@ -0,0 +1,19 @@
+//
+//  Refract.h
+//  Refract
+//
+//  Created by Kyle Fuller on 20/04/2015.
+//  Copyright (c) 2015 Cocode. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Refract.
+FOUNDATION_EXPORT double RefractVersionNumber;
+
+//! Project version string for Refract.
+FOUNDATION_EXPORT const unsigned char RefractVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Refract/PublicHeader.h>
+
+
